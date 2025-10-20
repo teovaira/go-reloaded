145a8be79a961dfb8da41f3e13e41a2fd4ac7648
tvairakt <t.vairaktaris@gmail.com>
2025-10-20 13:19:58 +0300

    docs: add file header to main.go

--- main.go
+++ main.go
@@ -1,3 +1,12 @@
+// -----------------------------------------------
+// Project: go-reloaded
+// File: main.go
+// Author: Theodore Vairaktaris
+// Description: Entry point for the go-reloaded project.
+// This program reads a text file, applies transformations,
+// and writes the modified content into another file.
+// -----------------------------------------------
+
 package main
 
 import (
